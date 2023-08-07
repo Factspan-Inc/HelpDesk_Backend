a0caf51f603d5f903633443eaa4264488435c4e0
ShivaChandrakanti <shiva.chandrakanti@factspan.com>
2023-08-07 15:06:42 +0530

    modified

--- help desk/helpdesk/ticketingsystem/ticketingsystem/all_drop_down.py
+++ help desk/helpdesk/ticketingsystem/ticketingsystem/all_drop_down.py
@@ -16,10 +16,17 @@ class All_dropdown():
             user_class_df = pd.read_csv(filepath_or_buffer=self.storage_path+"user_type.csv")
             ticket_types_df['TICKET_DESCRIPTION'] = ticket_types_df['TICKET_DESCRIPTION'].apply(lambda x:x.split(","))
             user_class_df['ACCESS_TYPE'] = user_class_df['ACCESS_TYPE'].apply(lambda x:x.split(","))
+            user_class_df['USER_CLASS_ID']=user_class_df['USER_CLASS_ID'].astype(str)
+            user_data_df = pd.read_csv(filepath_or_buffer=self.storage_path+"user_details.csv")
+            user_data_df['USER_CLASS_ID']=user_data_df['USER_CLASS_ID'].astype(str)
+            
+            user_data_df = pd.merge(left=user_data_df,right=user_class_df,how='inner',on='USER_CLASS_ID')
+    
 
             prepared_data = {
                 "USER_CLASS":user_class_df.to_dict(orient="records"),
-                "TICKET_CLASS":ticket_types_df.to_dict(orient="records")
+                "TICKET_CLASS":ticket_types_df.to_dict(orient="records"),
+                "USER_DETAILS":user_data_df.to_dict(orient="records")
             }
             return prepared_data
         except Exception as err:
